c4f3bbfa969864da6553263269ced3f2715fb046
manikandan m <kandanmani070@gmail.com>
2024-03-14 14:12:32 +0530

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DocumentData, QuerySnapshot } from '@firebase/firestore';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebase-crud';
+
+  studentDetails = {
+    name: '',
+    age: ''
+  }
+  studentCollectiondata: { id: string, name: string, age: string }[] | any = [];
+
+  constructor(private firebaseService: AppService) { }
+
+  ngOnInit(): void {
+    this.get();
+    this.firebaseService.obsr_UpdatedSnapshot.subscribe((snapshot) => {
+      this.updateStudentCollection(snapshot);
+    })
+  }
+
+  async add() {
+    const { name, age } = this.studentDetails;
+    await this.firebaseService.addStudent(name, age);
+    this.studentDetails.name = "";
+    this.studentDetails.age = "";
+  }
+
+  async get() {
+    const snapshot = await this.firebaseService.getStudents();
+    this.updateStudentCollection(snapshot);
+  }
+
+  updateStudentCollection(snapshot: QuerySnapshot<DocumentData>) {
+    this.studentCollectiondata = [];
+    snapshot.docs.forEach((student) => {
+      this.studentCollectiondata.push({ ...student.data(), id: student.id });
+    })
+  }
+
+  async delete(docId: string) {
+    await this.firebaseService.deleteStudent(docId);
+  }
+
+  async update(docId: string, name: HTMLInputElement, age: HTMLInputElement) {
+    await this.firebaseService.updateStudent(docId, name.value, age.value);
+  }
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCurTPc6L5JRI_-eDdnws2E6LGmTyBXyqQ",
+        authDomain: "sample-crudapp.firebaseapp.com",
+        projectId: "sample-crudapp",
+        storageBucket: "sample-crudapp.appspot.com",
+        messagingSenderId: "867464794361",
+        appId: "1:867464794361:web:c5f7970cc48ddbea945228"
+      },
+};
